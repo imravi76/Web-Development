5f7d8e56c5a2c58797760bc6a57bbee5f5add71b
Ravi Singh <40055802+imravi76@users.noreply.github.com>
2020-06-29 11:11:42 +0530

    JS Files update

---
+++ JSExample.js
@@ -0,0 +1,59 @@
+function myFunction(){
+    document.
+    getElementById("demo")
+    .innerHTML = 
+    "Paragraph Changed.";
+}
+
+Keywords
+
+break
+continue
+do
+while
+for
+function
+if
+else
+return
+switch
+try
+catch
+var
+
+var x,y,z   //How to declare variables
+
+x=5; y=6;   //How to assign values
+
+z=x+y       //How to compute values
+
+10.50  "John Doe"
+
+var x;
+x=6;
+x=5;    //
+
+var lastname, lastName;
+lastName = 0
+lastname = 1
+
+console.log(lastname)
+console.log(lastName)
+
+
+FirstName
+first_name
+firstName
+
+/*the,.l,s
+comment
+nk,s
+*/
+
+//Variables
+
+var x="5.0";
+var y=6;
+var z=x+y;
+ 
+int float double Boolean
