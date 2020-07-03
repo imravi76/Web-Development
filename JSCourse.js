74591c7d425bc45e1361ded4028f683ca409722f
Ravi Singh <40055802+imravi76@users.noreply.github.com>
2020-07-03 10:03:20 +0530

    JavaScript Arrays Files

--- JSCourse.js
+++ JSCourse.js
@@ -27,3 +27,25 @@ var car = "Fiat";
 var car = {type:"Fiat", model:"500", color:"white"};
 
 var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+
+<element event="some javascript"
+
+
+Example
+
+
+<button onclick="document.getElementById('demo').innerHTML=Date()">The Time is</button>
+
+var txt = "ABCD";
+
+var len = txt.length;
+
+var pos = txt.indexOf("C");
+
+var last = txt.lastIndexOf("E");
+
+var cars = ["saab", "volvo", "BMW"];
+
+var name = cars[0];
+
+print name
